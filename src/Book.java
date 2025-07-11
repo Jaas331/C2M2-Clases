ae49682181163604cfcc522de3c75144b5ef7565
Johan Abello <jabellonjaas@gmail.com>
2025-07-10 20:18:49 -0500

    error

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class Book {
+    // Atributos
+    String tittle;
+    String author;
+    Integer pages;
+    LocalDate publishiDate;  // nuevo metodo para manejar fechas
+    String editor;
+    String category;
+
+    // Constructor
+    // Metodos
+    
+}
