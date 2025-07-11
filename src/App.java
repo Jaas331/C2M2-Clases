8b3e9ea0852149266edde5c2b08049ed5a716e13
Johan Abello <jabellonjaas@gmail.com>
2025-07-10 22:12:14 -0500

    completo de clase

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Book book1 = new Book();
+        var book2 = new Book("Programacion en java", "Pedro Martinez", 900);
+        var book3 = new Book("Lengua inglesa", "Jonh Dog", 500, LocalDate.of(2015, 10, 21), "Englis chool", "Aprendiz");
+    
+    System.out.println(book1);
+    System.out.println(book2);
+    System.out.println(book3);
+    
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -2,14 +2,66 @@ import java.time.LocalDate;
 
 public class Book {
     // Atributos
-    String tittle;
-    String author;
-    Integer pages;
-    LocalDate publishiDate;  // nuevo metodo para manejar fechas
-    String editor;
-    String category;
+    private String title;
+    private String author;
+    private Integer pages;
+    private LocalDate publishiDate;  // nuevo metodo para manejar fechas
+    private String editor;
+    private String category;
 
-    // Constructor
+    // Constructor, inicializa un objeto
+    public Book(){
+        title = "Las aventuras de cesar";
+        author = "Cesar Diaz";
+        pages = 10;
+        publishiDate = LocalDate.now();
+        editor = "McGrawHill";
+        category = "Action";
+
+    }
+
+    public Book(String title, String author, Integer pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.publishiDate = LocalDate.now();
+        this.editor = "McGrawHill";
+        this.category = "Action";
+    }
+
+        public Book(String title, String author, Integer pages, LocalDate publishDate, String editor, String category) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.publishiDate = publishiDate;
+        this.editor = editor ;
+        this.category = category;
+        }
+
     // Metodos
+    public  String dameTuTitulo(){
+        return this.title;
+    }
+
+    // Metodos Getter -> Obtener los valores de los atributos
+    public String getTitle(){
+        return title;
+    }
     
+    public String getAuthor(){
+        return author;
+    }
+
+    public String getpages(){
+        return author;
+    }
+
+    // Setter -> cambiar de manera directa el valor de un atributo -> set<Atributo>(TD par)
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public String toString(){
+        return "Libro: titulo: " + title;
+    }
 }
